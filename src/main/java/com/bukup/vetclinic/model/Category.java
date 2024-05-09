6a9e1f4b8e269d329bcc16ecad0c835a991a8649
Matvii Butenov <matvej1888@gmail.com>
2024-05-10 00:11:19 +0200

    adjust persistence layer: add new Entity ServiceType

--- src/main/java/com/bukup/vetclinic/model/Category.java
+++ src/main/java/com/bukup/vetclinic/model/Category.java
@@ -2,14 +2,7 @@ package com.bukup.vetclinic.model;
 
 import java.util.Set;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -29,4 +22,7 @@ public class Category
 
 	@ManyToMany(mappedBy = "categories", fetch = FetchType.LAZY)
 	private Set<Employee> employees;
+
+	@OneToMany(mappedBy = "category")
+	private Set<ServiceType> serviceTypes;
 }
---
+++ src/main/java/com/bukup/vetclinic/model/ServiceType.java
@@ -0,0 +1,23 @@
+package com.bukup.vetclinic.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "service_types")
+public class ServiceType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
