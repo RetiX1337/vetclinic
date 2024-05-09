69467a2a2e68dffeaf0509d8b590d79f02e88933
Matvii Butenov <matvej1888@gmail.com>
2024-05-10 00:12:06 +0200

    add ServiceTypeRepository

--- src/main/java/com/bukup/vetclinic/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bukup.vetclinic.repository;
-
-import com.bukup.vetclinic.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-}
